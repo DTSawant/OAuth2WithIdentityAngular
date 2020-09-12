2ad8fd9e348527840b0d52d239e4948960711392
DTSawant <51373596+DTSawant@users.noreply.github.com>
2020-09-12 18:13:45 +0530

    IdentityServer part files

---
+++ src/IdentityServer/Quickstart/TestUsers.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "818727",
+                        Username = "user1",
+                        Password = "user1",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "User1"),
+                            new Claim(JwtClaimTypes.GivenName, "User 1"),
+                            new Claim(JwtClaimTypes.FamilyName, "User"),
+                            new Claim(JwtClaimTypes.Email, "user1@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://user1.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "88421113",
+                        Username = "user2",
+                        Password = "user2",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "User 2"),
+                            new Claim(JwtClaimTypes.GivenName, "User 2"),
+                            new Claim(JwtClaimTypes.FamilyName, "User"),
+                            new Claim(JwtClaimTypes.Email, "user2@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://user2.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
